1c6cab721b6f5abda9426d062293b5ca39393309
echizhevskiy <egorkachi@mail.ru>
2015-03-31 18:57:36 +0200

    lab№2 has been added

---
+++ lab№2/myfunc.h
@@ -0,0 +1,6 @@
+void inputarray(int **mas, int m );
+void initarray(int **mas, int m );
+void printarray(int **mas, int m );
+int  MinElement(int **mas, int m );
+void changesign(int **mas, int m, int k);
+int printcondition();
